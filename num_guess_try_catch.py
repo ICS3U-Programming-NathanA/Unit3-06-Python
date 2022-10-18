cf0b391a6a2270f193239f9838629d93bf3e044d
Nathan Araujo <nathan.araujo@stu.ocsb.ca>
2022-10-18 03:32:22 +0000

    This program asks the user for a number between 0 and 9 and then sees if they got the same number as the computer

---
+++ num_guess_try_catch.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# Created by: Nathan Araujo
+# Created on: Oct 17 2022
+# This program asks the user for a number between 0 and 9 and then sees if they got the same number as the computer
+
+
+from logging import exception
+import random
+
+
+def main():
+    # generates a number between 0 and 9
+    random_num = random.randint(0, 9)
+
+    # Get the the number they guessed
+    user_num = input("Enter a number between 0 and 9: ")
+
+    # An If statement to see if they got the same number as the generated number then display the results
+    if random_num == user_num:
+        print("You guessed the correct number")
+    else:
+        print("You guessed the incorrect number")
+
+    try:
+        user_num_as_a_number = int(user_num)
+        print("")
+    except ValueError:
+        print("This number is not a integer")
+    finally:
+        print("Thanks for playing")
+
+
+if __name__ == "__main__":
+    main()
